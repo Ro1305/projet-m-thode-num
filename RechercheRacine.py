e6a54f511dc32eb116bbe86f18781bb628f66945
Robin Gérard <88779667+Ro1305@users.noreply.github.com>
2026-03-14 18:53:14 +0100

    Add files via upload

--- RechercheRacine.py
+++ RechercheRacine.py
@@ -5,47 +5,48 @@ def bissection (f, x0, x1, tol):
     if fx0*fx1 > 0:
         print("Erreur : Les ordonnées des valeurs fournies ne sont pas de signe opposé")
         return [x0, 1]
-    if fx0 == 0:
+    if abs(fx0) < tol:
         return [x0, 0]
-    if fx1 == 0:
+    if abs(fx1) < tol:
         return [x1, 0]
-    r = abs(fx1-fx0)
-    
-    while r>tol:
+    x2 = (x0+x1)/2
+    while abs(x0-x1)>tol:
         x2 = (x0+x1)/2
         fx2 = f(x2)
-        if fx2 == 0:
+        if abs(fx2) < tol:
             return [x2, 0]
         if fx0*fx2>0:
             x0 = x2
             fx0 = fx2
         else :
             x1 = x2
-            fx1 = fx2
-        
-        r = abs(fx1-fx0)
+            fx1 = fx2        
     return [x2, 0]
 
 def secante (f, x0, x1, tol):
     fx0 = f(x0)
     fx1 = f(x1)
     
-    if fx0 == 0:
+    if abs(fx0)<tol:
         return [x0, 0]
-    if fx1 == 0:
+    if abs(fx1)<tol:
         return [x1, 0]
-    r = abs(fx1-fx0)
+    if abs(fx1-fx0)<tol:
+        print("Erreur")
+        return[x0, 1]
+    
     itérations = 0
-    while r>tol:
+    while abs(x1-x0)>tol:
         if fx1 - fx0 == 0:
             print("Erreur : Division par zéro (pente nulle)")
             return [x0, -1]
-        x2 = x0 - fx0*(x1-x0)/(fx1-fx0)
+        x2 = x1 - fx1*(x1-x0)/(fx1-fx0)
         x0, fx0 = x1, fx1
         x1, fx1 = x2, f(x2)
-        if fx1 == 0:
+        
+        if abs(fx1) < tol:
             return [x1, 0]
-        r = abs(fx1-fx0)
+        
         itérations+=1
         if itérations > 500:
             print("Erreur : Limite d'itérations atteinte")
--- SimRayon.py
+++ SimRayon.py
@@ -1,14 +1,14 @@
-import numpy as np
+import numpy as np 
 from scipy import integrate
 import matplotlib.pyplot as plt
+from RechercheRacine import bissection
 
 def odefunction(x, y, profilTemperature):
-    ns = 1.000272983820855
     Ts = 288.15
+    ns = 1.000272983820855
     z = y[0]
     i = y[1]
     T, dTdz = profilTemperature(z)
-    
     dy = np.zeros(2)
     dy[0] = np.tan(i)
     dy[1] = (-(ns-1)*(Ts/T**2)*dTdz)*(1/(1+(ns-1)*(Ts/T)))
@@ -29,22 +29,79 @@ def trajetRayonEuler(intervalle, y0, dx, profilTemperature):
         
         x.append(xs)
         Y.append(ys)
-    
-    return np.array(x), np.array(Y)
-    
-def trajetRayonIVP(intervalle, y0, rtol, profilTemperature):
-    sol = integrate.solve_ivp(odefunction, intervalle, y0, args=(profilTemperature, ), rtol=rtol)
-    return [sol.t, sol.y]
+    return np.array(x), np.array(Y).T
 
 def profilTemperatureLin(z):
     h = 0.5
-    Tsol = 30 + 273.15
-    Th = 15 + 273.15
-    
-    if z < h:
-        dTdz = (Th - Tsol)/h
-        T = Tsol + dTdz * z
-    else: 
+    Th = 288.15
+    Tsol = 303.15
+    if z < h :
+        T = ((Th-Tsol)/h)*z+Tsol
+        dTdz = (Th-Tsol)/h
+    else :
         T = Th
-        dTdz = 0.0 
+        dTdz = 0
     return T, dTdz
+
+def trajetRayonIVP(intervalle, y0, rtol, profilTemperature):
+    sol = integrate.solve_ivp(odefunction, intervalle, y0, rtol=rtol, args=(profilTemperature, ))
+    return sol.t, sol.y
+
+
+
+#x_ivp, Y_ivp = trajetRayonIVP([0, 1000], [1, np.radians(-0.5)], 10**-10, profilTemperatureLin)
+
+"""for o in np.linspace(-1, 0.1, 20):
+    x_eul, Y_eul = trajetRayonEuler([0, 1000], [1, np.radians(o)], 0.01, profilTemperatureLin)
+    z_eul = Y_eul[0,:]
+    ss = np.where(z_eul<=0)[0]
+    if len(ss)>0:
+        indice_sol = ss[0]
+        x_positif = x_eul[:indice_sol + 1]
+        z_positif = z_eul[:indice_sol + 1]
+        plt.plot(x_positif, z_positif)
+    else:
+        plt.plot(x_eul, z_eul)
+
+plt.show()"""
+#z_ivp = Y_ivp[0, :]
+"""zf_ivp = Y_ivp[0, -1]
+zf_eul = Y_eul[0, -1]
+erreur = abs(zf_ivp-zf_eul)
+print(erreur)"""
+#plt.plot(x_ivp, z_ivp)
+
+intervalle = [0, 1000]
+z0 = 1
+xf = 1000
+zf = 1.5
+def ecart_altitude(i0_deg, xf, z0, zf):
+    i0_rad = np.radians(i0_deg)
+    x, Y = trajetRayonEuler(intervalle, [z0, i0_rad], 0.1, profilTemperatureLin)
+    zf_fin = Y[0, -1]
+    return zf_fin - zf
+
+def f_a_resoudre(angle):
+    return ecart_altitude(angle, xf, z0, zf)
+
+resultat1 = bissection(f_a_resoudre, -0.5, 0.0, 10**-10)
+resultat2 = bissection(f_a_resoudre, 0, 0.1, 10**-10)
+i01 = resultat1[0]
+statut1 = resultat1[1]
+i02 = resultat2[0]
+statut2 = resultat2[1]
+
+x_eul, Y_eul = trajetRayonEuler([0, 1000], [z0, np.radians(i01)], 0.1, profilTemperatureLin)
+z_eul = Y_eul[0,:]
+print(i01,", statut:",statut1)
+plt.plot(x_eul, z_eul)
+
+    
+x_eul, Y_eul = trajetRayonEuler([0, 1000], [z0, np.radians(i02)], 0.1, profilTemperatureLin)
+z_eul = Y_eul[0,:]
+plt.plot(x_eul, z_eul)
+plt.plot(x_eul, z_eul)
+print(i02,", statut",statut2)
+plt.show()
+
+    
