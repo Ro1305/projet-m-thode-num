c4ce03612a13cf63769825a661949973b86e120b
Robin Gérard <88779667+Ro1305@users.noreply.github.com>
2026-04-04 13:14:54 +0200

    Add files via upload

--- question3.py
+++ question3.py
@@ -1,25 +1,34 @@
 import numpy as np 
 from SimRayon import trajetRayonEuler,profilTemperatureLin,trajetRayonIVP
 from RechercheRacine import bissection
+import matplotlib.pyplot as plt
 
-def nomcool(intervalle , z0 , xf , zf):
+def recherche_angles(intervalle , z0 , xf , zf):
     # angle direct
     # atan2 arc tangent of x1/x2
-    i02= np.atan2(zf-z0, xf)
+    i02= np.atan2(abs(zf-z0), xf)
 
-    def ecart_altitude(i0, z0, zf):
-        # _ pour ne pas prendre en compte x
-        _, Y = trajetRayonIVP(intervalle, [z0, i0], 1e-7, profilTemperatureLin,sol=True)
+    def ecart_altitude(i0, z0,xf, zf):
+        x, Y = trajetRayonEuler(intervalle, [z0, i0], 1, profilTemperatureLin,sol=True)
         zf_fin = Y[0, -1]
-        pomme=  zf_fin - zf
-        return pomme
+        if x[-1] < xf: 
+            return 5
+        return zf_fin- zf
+        
 
     def f_a_resoudre(angle):
-        return ecart_altitude(angle, z0, zf)
+        return ecart_altitude(angle, z0,xf, zf)
     decalage= 1e-5
     seuil= np.pi/2 - decalage
-    # on suppose que le phenomene a lieu en dessous de 1 degree
-    x= - np.pi
+    intervalle_inf = [-seuil , i02 -decalage ]
+    resultat = bissection(f_a_resoudre, intervalle_inf[0], intervalle_inf[1], 1e-4)
+    if resultat[1] == 1:
+            intervalle_supp= [-0.1,seuil]
+            resultat= bissection(f_a_resoudre,intervalle_supp[0],intervalle_supp[1],1e-4)
+    
+    return i02, resultat[0]
+if __name__ == '__main__':    
+    """x= - np.pi
 
     k= 1
     max =0
@@ -29,21 +38,18 @@ def nomcool(intervalle , z0 , xf , zf):
     # cf le graph
     while(k== 1 and max <100):
         x/=2
-        resultat= bissection(f_a_resoudre,x,intervalle_inf[1],1e-7)
+        resultat= bissection(f_a_resoudre,x,intervalle_inf[1],1e-4)
         k= resultat[1]
         max+=1
-    
-    print(resultat)
     
     if resultat[1] == 1:
         # todo reimplementer la meme approche ici pour une déflexion par le haut
         intervalle_supp= [i02-decalage,seuil]
-        resultat= bissection(f_a_resoudre,intervalle_supp[0],intervalle_supp[1],1e-7)
-
-
-    return [i02,resultat[0]]
+        resultat= bissection(f_a_resoudre,intervalle_supp[0],intervalle_supp[1],1e-4)
+    return [i02, resultat[0]]"""
+"""   
 def q32(intervalle , z0 , xf , zf):
-    direct, indirect=nomcool(intervalle , z0 , xf , zf)
+    direct, indirect=recherche_angles(intervalle , z0 , xf , zf)
     x, Y2 = trajetRayonEuler(intervalle, [z0, indirect], 1e-3, profilTemperatureLin)
     ooo = Y2[1]-direct
     # todo plot les 2 euler au meme point
@@ -53,39 +59,42 @@ def q32(intervalle , z0 , xf , zf):
     from matplotlib import pyplot as plt
     plt.plot(ooo)
     plt.show()
-    return np.max(np.asb(ooo))
+    return np.max(np.abs(ooo))"""
 
     
 
-intervalle = [0, 1000]
 z0 = 1
 xf = 1000
+intervalle = [0, xf]
 zf = 1.5
+recherche_angles(intervalle , z0 , xf , zf)
+#print(q32(intervalle, z0 , xf, zf))
+i02, resultat = recherche_angles(intervalle , z0 , xf , zf)
+print(i02, resultat)
 
-print(q32(intervalle, z0 , xf, zf))
-"""
 
-def ecart_altitude(i0, z0, zf):
+def ecart_altitude(i0, z0,xf, zf):
     # _ pour ne pas prendre en compte x
-    _, Y = trajetRayonIVP(intervalle, [z0, i0], 1e-7, profilTemperatureLin,sol=True)
+    x, Y = trajetRayonEuler(intervalle, [z0, i0], 1, profilTemperatureLin,sol=True)
     zf_fin = Y[0, -1]
-    pomme=  zf_fin - zf
-    return pomme
-AAAAAAAAAA = np.linspace(-0.01,0.01,100)
+    if x[-1] < xf: 
+        return 5
+    return zf_fin - zf
+AAAAAAAAAA = np.linspace(-0.02,0.01,100)
 BB = np.empty_like(AAAAAAAAAA)
 for i in range(len(AAAAAAAAAA)):
-    BB[i]=ecart_altitude(AAAAAAAAAA[i] , z0 ,zf )
+    BB[i]=ecart_altitude(AAAAAAAAAA[i] , z0,xf,zf)
 
 from matplotlib import pyplot as plt
 i02= np.atan2(zf-z0, xf)
 decalage= 1e-5
 plt.axhline(0)
 plt.plot(AAAAAAAAAA,BB)
-u = nomcool(intervalle, z0 , xf, zf)
-plt.axvline(u[0])
+u = recherche_angles(intervalle, z0 , xf, zf)
+#plt.axvline(u[0])
 plt.axvline(u[1])
 plt.show()
-
+"""
 i01 = resultat1[0]
 statut1 = resultat1[1]
 #########################
